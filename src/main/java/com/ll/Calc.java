886410c75fd8e36ae98b4ea8f23a1fd649ee47f9
yhwit30 <yhwit30@gmail.com>
2023-12-27 13:58:31 +0900

    + * 복합연산 구현. 빼기연산 버그

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -3,18 +3,36 @@ package com.ll;
 public class Calc {
   public static int run(String exp) {
 
-    exp = exp.replaceAll("- ", "\\+ -");
+    boolean needToMulti = exp.contains("*");
     boolean needToPlus = exp.contains("+");
+    boolean needToCompund = needToMulti && needToPlus;
 
-    String[] bits = exp.split(" \\+ ");
 
-    int sum = 0;
-    for (int i = 0; i < bits.length; i++) {
-      sum += Integer.parseInt(bits[i]);
+    if (needToCompund) {
+      exp = exp.replaceAll("- ", "\\+ -");
+      String[] bits = exp.split(" \\+ ");
+      String[] bits2 = bits[1].split(" \\* ");
+
+      return Integer.parseInt(bits[0]) + run(bits[1]);
+    } else if (needToMulti) {
+      String[] bits = exp.split(" \\* ");
+
+      int result = 1;
+      for (int i = 0; i < bits.length; i++) {
+        result *= Integer.parseInt(bits[i]);
+      }
+      return result;
+    } else if (needToPlus) {
+      exp = exp.replaceAll("- ", "\\+ -");
+      String[] bits = exp.split(" \\+ ");
+      int sum = 0;
+      for (int i = 0; i < bits.length; i++) {
+        sum += Integer.parseInt(bits[i]);
+      }
+      return sum;
     }
-    return sum;
 
 
-//    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
+    throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
   }
 }
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,6 +3,14 @@ package com.ll;
 public class Main {
   public static void main(String[] args) {
 
+    int a = 3;
+    int b = 4;
+
+    int sum = 0;
+        for(int i = 0; i < b; i++){
+          sum += a;
+        }
+    System.out.println(sum);
 
   }
 }
